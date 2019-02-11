8ed9911fadcb623a005eb9a43bb65b5524f22574
Jonathan <jonathan@sandbox.com>
2019-02-11 22:19:05 +0100

    Update Producer and driver to produce syncronously and asyncronously

--- Kafka/src/main/java/producers/ProducerDriver.java
+++ Kafka/src/main/java/producers/ProducerDriver.java
@@ -7,12 +7,19 @@ import java.util.stream.Stream;
 public class ProducerDriver {
 
     public static void main(String[] args) {
+        String errMsg = "Expected Usage: ProducerDiver ['async or sync', 'topic', 'msg1', 'msg2', ...]";
         if (args.length <= 1) {
-            throw new IllegalStateException("Expected Usage: ProducerDiver ['topic', 'msg1', 'msg2', ...]");
+            throw new IllegalStateException(errMsg);
         }
 
-        SimpleProducer simpleProducer = new SimpleProducer(args[0]);
+        SimpleProducer simpleProducer = new SimpleProducer(args[1]);
         simpleProducer.configure("localhost:9092");
-        Stream.of(args).skip(1).map(simpleProducer::produce).forEach(r -> System.out.println(r.toString()));
+        if (args[0].equals("async")) {
+            Stream.of(args).skip(1).map(simpleProducer::asyncProduce).forEach(r -> System.out.println(r.toString()));
+        } else if (args[0].equals("sync")) {
+            Stream.of(args).skip(1).map(simpleProducer::syncProduce).forEach(r -> System.out.println(r.toString()));
+        } else {
+            throw new IllegalArgumentException(errMsg);
+        }
     }
 }
--- Kafka/src/main/java/producers/basic/SimpleProducer.java
+++ Kafka/src/main/java/producers/basic/SimpleProducer.java
@@ -1,11 +1,14 @@
 package producers.basic;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
+import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class SimpleProducer {
     private Properties kafkaProps = new Properties();
@@ -23,7 +26,7 @@ public class SimpleProducer {
         producer = new KafkaProducer<>(kafkaProps);
     }
 
-    public RecordMetadata produce(String msg) {
+    public RecordMetadata syncProduce(String msg) {
         ProducerRecord<String, String> record = new ProducerRecord<>(this.topic, msg);
         RecordMetadata recordMetadata = null;
         try {
@@ -33,5 +36,21 @@ public class SimpleProducer {
         }
 
         return recordMetadata;
+    }
+
+    public Future<RecordMetadata> asyncProduce(String msg) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(this.topic, msg);
+        return producer.send(record, new SimpleCallBack());
+    }
+
+    private class SimpleCallBack implements Callback {
+
+        @Override
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+            if (Objects.nonNull(exception)) {
+                System.out.println("Error Writing to topic: " + metadata.topic());
+                exception.printStackTrace();
+            }
+        }
     }
 }
