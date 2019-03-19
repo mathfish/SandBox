7dfe21a9316b8d87b013f75bc8a72139fac78174
Jonathan <jonathan@Fugu.com>
2019-03-19 23:57:09 +0100

    Initial consumer

--- Kafka/src/main/java/consumers/proto/ListingConsumer.java
+++ Kafka/src/main/java/consumers/proto/ListingConsumer.java
@@ -2,15 +2,28 @@ package consumers.proto;
 
 import com.basic.ListingKey;
 import com.basic.ListingValue;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
-import scala.sys.Prop;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Objects;
 import java.util.Properties;
 
+
+@Slf4j
 public final class ListingConsumer implements Runnable{
 
-    private ListingConsumer() {
+    private final Consumer<ListingKey, ListingValue> consumer;
 
+    public ListingConsumer(Integer clientId) {
+        Properties consumerProperties = getConsumerProperties(clientId);
+        consumer = new KafkaConsumer<>(consumerProperties);
+        consumer.subscribe(Collections.singleton("Airbnb_Listings"));
     }
 
     private Properties getConsumerProperties(Integer id) {
@@ -24,14 +37,44 @@ public final class ListingConsumer implements Runnable{
         return consumerProps;
     }
 
-    Consumer<ListingKey, ListingValue> makeListingConsumer(int id) {
-
-
-        return null;
-    }
-
     @Override
     public void run() {
-        
+        try {
+            while (true) {
+                ConsumerRecords<ListingKey, ListingValue> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<ListingKey, ListingValue> record : records) {
+                    log.info(buildMessage(record));
+                }
+
+                consumer.commitAsync((m,e) -> {
+                    if (Objects.nonNull(e)) {
+                        log.warn("Failure to commit message:\n " + m, e);
+                    }
+                });
+            }
+
+        } catch (WakeupException exception) {
+            try {
+                consumer.commitSync();
+            } finally {
+                consumer.close();
+            }
+        }
+    }
+
+    public void shutdown() {
+        consumer.wakeup();
+    }
+
+    private String buildMessage(ConsumerRecord<ListingKey, ListingValue> record) {
+        String msg = "Topic " + record.topic() +
+                "- partition " + record.partition() +
+                "- offset" + record.offset() +
+                "\nHeader: " + record.headers() +
+                "\nKey: " + record.key() +
+                "\nListingId: " + record.value().getListingId() +
+                ", Num Reviews: " + record.value().getNumberReviews() +
+                ", Rating: " + record.value().getRating();
+        return msg;
     }
 }
