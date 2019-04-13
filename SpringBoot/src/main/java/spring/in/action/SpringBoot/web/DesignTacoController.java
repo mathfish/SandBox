114d757502c8f9b106a0b808bda0da370f66802d
Jonathan <jonathan@sandbox.com>
2019-04-13 21:15:57 +0200

    Add model attribute for taco for design Was getting error for request atrribte being 'taco' and not 'design' after post

--- SpringBoot/src/main/java/spring/in/action/SpringBoot/web/DesignTacoController.java
+++ SpringBoot/src/main/java/spring/in/action/SpringBoot/web/DesignTacoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.in.action.SpringBoot.Ingredient;
@@ -53,7 +54,7 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
         if (errors.hasErrors()) {
             return "design";
         }
