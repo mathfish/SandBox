8968e10d10ff4e8f0a41d1f8eb6e9d92b81ffff7
Jonathan <jonathan@sandbox.com>
2019-05-02 08:02:58 +0200

    Add controller logic for order saving and repo as bean. Potentially chapter 3 JdcbcTemplate version done

--- SpringBoot/src/main/java/spring/in/action/SpringBoot/data/JdbcTacoRepository.java
+++ SpringBoot/src/main/java/spring/in/action/SpringBoot/data/JdbcTacoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import spring.in.action.SpringBoot.Ingredient;
+import org.springframework.stereotype.Repository;
 import spring.in.action.SpringBoot.Taco;
 
 import java.sql.Timestamp;
@@ -14,6 +14,7 @@ import java.sql.Types;
 import java.util.Arrays;
 import java.util.Date;
 
+@Repository
 public class JdbcTacoRepository implements TacoRepository {
 
     private final JdbcTemplate jdbc;
